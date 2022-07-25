9fade69efa35f96d5786cc9d424244ef1988ef3f
Jacob Adam <87478152+jijlej@users.noreply.github.com>
2022-07-25 16:11:33 +0800

    Update USBDAQ-V12-test0.py

--- USBDAQ-V12-test0.py
+++ USBDAQ-V12-test0.py
@@ -1,25 +1,84 @@
+import time
 import numpy as np
-from scipy.fftpack import fft,ifft
-from scipy import fftpack
+from ctypes import *
+from pyqtgraph.Qt import QtCore,QtGui
+import pyqtgraph as pg
+from pyqtgraph.widgets.RemoteGraphicsView import RemoteGraphicsView
+from pyqtgraph import examples
 
-freq = 40*1000 #参数输入模块
+micronum=3
+freq=40*1000  #采样频率，单位赫兹
+fps = 10  #帧率，单位图/秒
+total_duration = 10 #总采样时长，单位秒
+total_duration=total_duration-1
+num = freq//fps #每张图点数，整数制
+pic_num = fps*total_duration #总生成图数
+print("每张图点数:"+str(num))
+print("总图数："+str(pic_num))
 
-def time_est(x,y):#时间延迟估计函数，x和y分别是采样数组，注意是数组
-    sample_freq = fftpack.fftfreq(x.size,1/freq)
-    xf = fft(x)
-    yf = fft(y)
-    for i in range(len(yf)):
-        yf[i] = yf[i].conjugate() #取共扼运算
-    Rxy = []
-    for i in range(len(yf)):
-        a = xf[i]*yf[i]
-        Rxy.append(a)
-    Rxy = np.array(Rxy)
-    Rxy = ifft(Rxy)
-    tidelay = Rxy.argmax()
-    return tidelay
+#stdcall调用约定：两种加载方式
+#DAQdll = ctypes.windll.LoadLibrary("dllpath")
+#cdecl调用约定：两种加载方式
+#DAQdll = ctypes.cdll.LoadLibrary("dllpath")
+#DAQdll = ctypes.CDLL("dllpath")
+#<span style="font-family:Microsoft YaHei;">
 
-t = np.arange(0,0.2,1/freq) #测试数据生成与处理
-test1 = 7*np.sin(2*np.pi*180*t) + 2.8*np.sin(2*np.pi*390*t)
-test2 = 7*np.sin(2*np.pi*180*(t+0.01)) + 2.8*np.sin(2*np.pi*390*(t+0.01))
-t_delay = time_est(test1,test2)/freq
+#首先打开设备
+
+
+#由于int __stdcall ADSingleV12(int ad_mod, int chan, int gain, float* adResult);
+#函数需要返回一个采集结果，使用float*传入一个地址，采集结果写入这个指针所指向的地址，
+#所以需要先申明一个float类型的变量，然后使用byref得到这个变量地址当做指针传给函数
+#调用函数采集通道1的电压，单端模式，量程默认正负10V
+#打印采集到的电压值
+
+
+app = pg.mkQApp("Plot Auto Range Example")
+win = pg.GraphicsLayoutWidget(show=True, title="Plot auto-range examples")
+win.resize(800,600)
+win.setWindowTitle('pyqtgraph example: PlotAutoRange')
+p0 = win.addPlot(title="Auto Pan Only")
+p0.setAutoPan(y=True)
+p1=win.addPlot(title="Auto Pan Only")
+p1.setAutoPan(y=True)
+p2=win.addPlot(title="Auto Pan Only")
+p2.setAutoPan(y=True)
+curve0 = p0.plot()
+curve1=p1.plot()
+curve2=p2.plot()
+def update():
+    print(DAQdll.GetAdBuffSizeV12())
+    T=[]*num
+    if (DAQdll.GetAdBuffSizeV12() >= num*micronum):
+        value0=[]
+        value1=[]
+        value2=[]
+        DAQdll.ReadAdBuffV12(byref(advalue), num*micronum)
+        for i in range(3):
+            for j in range(num):
+                if i==0:
+                    value0.append(advalue[i][j])
+                if i==1:
+                    value1.append(advalue[i][j])
+                else:
+                    value2.append(advalue[i][j])
+        curve0.setData(value0)
+        curve1.setData(value1)
+        curve2.setData(value2)
+
+allvalue=[]*freq*(total_duration+1)
+T = []
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(10)
+
+if __name__ == '__main__':
+    DAQdll = WinDLL("./USBDAQ_DLL_V12X64.dll")
+    erro = DAQdll.OpenUsbV12()
+    advalue = (c_float * int(num) * micronum)()
+    erro = DAQdll.ADSingleV12(1, 0, 1, byref(advalue))
+    print(advalue)
+    erro = DAQdll.MADContinuConfigV12(1, 0, micronum-1, 1 , freq)
+    pg.exec()
+    erro = DAQdll.ADContinuStopV12()
+    erro = DAQdll.CloseUsbV12()
