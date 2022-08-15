b45380bd4d7671a358cfda63df865a0cc167da55
瓜皮 <98169199+SophistGuaPi@users.noreply.github.com>
2022-08-15 23:01:24 +0800

    时域互相关算法实现以及原程序一些错误修正

--- 时域互相关算法.py
+++ 时域互相关算法.py
@@ -9,7 +9,7 @@ from scipy.fftpack import fft,ifft
 from scipy import fftpack
 
 #参数输入
-micronum=3
+micronum=3 
 freq=40*1000  #采样频率，单位赫兹
 fps = 10  #帧率，单位图/秒
 total_duration = 10 #总采样时长，单位秒
@@ -18,6 +18,7 @@ num = freq//fps #每张图点数，整数制
 pic_num = fps*total_duration #总生成图数
 print("每张图点数:"+str(num))
 print("总图数："+str(pic_num))
+t0list,t1list,t2list=[],[],[]
 
 #stdcall调用约定：两种加载方式
 #DAQdll = ctypes.windll.LoadLibrary("dllpath")
@@ -40,49 +41,56 @@ app = pg.mkQApp("Plot Auto Range Example")
 win = pg.GraphicsLayoutWidget(show=True, title="Plot auto-range examples")
 win.resize(800,600)
 win.setWindowTitle('pyqtgraph example: PlotAutoRange')
-p0 = win.addPlot(title="AD1")
+p0 = win.addPlot(title="Auto Pan Only")
 p0.setAutoPan(y=True)
-p1=win.addPlot(title="AD2")
+p1=win.addPlot(title="Auto Pan Only")
 p1.setAutoPan(y=True)
-p2=win.addPlot(title="AD3")
+p2=win.addPlot(title="Auto Pan Only")
 p2.setAutoPan(y=True)
 win.nextRow()
-p3 = win.addPlot(title="FFT")
-p3.setAutoPan(y=True)
-win.nextRow()
-p4=win.addPlot(title='')
+p3 = win.addPlot(title="Auto Pan Only")
 curve0 = p0.plot()
 curve1=p1.plot()
 curve2=p2.plot()
 curve3 = p3.plot()
-curve4=p4.plot()
 
 #函数定义
-def time_est(x,y):#时间延迟估计函数，x和y分别是采样列表，注意是数组
-    sample_freq = fftpack.fftfreq(x.size,1/freq)
-    xf = fft(x)
-    yf = fft(y)
-    for i in range(len(yf)):
-        yf[i] = yf[i].conjugate() #取共扼运算
-    Rxy = []
-    for i in range(len(yf)):
-        a = xf[i]*yf[i]
-        Rxy.append(a)
-    Rxy = np.array(Rxy)
-    Rxy = ifft(Rxy)
-    curve4.setData(abs(Rxy))
-    tidelay = Rxy.argmax()
-    tidelay = tidelay/freq
-    return tidelay
+def Rxy(x,y,tran):#x是第一个信号列表，y是第二个信号列表,返回值将是dx-dy,tran是平移量，是
+    a = 0
+    b = 0
+    c = 0
+    if tran < 0:
+        #print(tran)
+        for i in range(len(x)-abs(tran)):
+            a+=x[i+abs(tran)]*y[i]
+            b = x[i+abs(tran)]**2
+            c = y[i]**2
+    else:
+        for i in range(len(x)-abs(tran)):
+            a+= x[i]*y[i+tran]
+            b = x[i]**2
+            c = y[i+tran]**2
+    d = a/(b*c)
+    return d
+def tdelay(x,y,num):
+    Rxylist = []
+    n=num*(-1)+1
+    for i in range(n,num):
+        Rxylist.append(Rxy(x,y,i))
+    m = max(Rxylist)
+    curve3.setData(Rxylist)
+    t = (Rxylist.index(m)-40)/(40000)
+    return t
+
 def update():
+    global t0list, t1list, t2list
     T=[]*num
     if (DAQdll.GetAdBuffSizeV12() >= num*micronum):
-        print(DAQdll.GetAdBuffSizeV12())
         value0=[]
         value1=[]
         value2=[]
         DAQdll.ReadAdBuffV12(byref(advalue), num*micronum)
-        for i in range(2):
+        for i in range(3):
             for j in range(num):
                 if i==0:
                     value0.append(advalue[i][j])
@@ -90,37 +98,30 @@ def update():
                     value1.append(advalue[i][j])
                 if i==2:
                     value2.append(advalue[i][j])
-        value0=value0[-1::-1]
-        value1=value1[-1::-1]
-        value2 = value2[-1::-1]
         curve0.setData(value0)
         curve1.setData(value1)
         curve2.setData(value2)
-        value01 = np.array(value0)
-        value11 = np.array(value1)
-        value21 = np.array(value2)
-        sample_freq = fftpack.fftfreq(value01.size,1/freq)
-        value0f = fft(value01)
-        curve3.setData(abs(value0f))
-        t01 = time_est(value01,value11)
-        t12 = time_est(value11,value21)
-        print(t01,t12)
+        t10 = tdelay(value1,value0,num//100)
+        t20 = tdelay(value2,value0,num//100)
+        #t0list.append(t10)
+        #curve3.setData(t0list)
+        print(t10,t20)
+        
+
 
 #数据准备与初始化
-allvalue=[]*freq*(total_duration+1)
-T = []
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(10)
+
 #打开采集卡，连续采集与关闭
 if __name__ == '__main__':
     DAQdll = WinDLL("./USBDAQ_DLL_V12X64.dll")
     erro = DAQdll.OpenUsbV12()
     advalue = (c_float * int(num) * micronum)()
-    print(len(advalue))
     erro = DAQdll.ADSingleV12(1, 0, 1, byref(advalue))
     print(advalue)
     erro = DAQdll.MADContinuConfigV12(1, 0, micronum-1, 1 , freq)
-    timer = QtCore.QTimer()
-    timer.timeout.connect(update)
-    timer.start(10)
     pg.exec()
     erro = DAQdll.ADContinuStopV12()
     erro = DAQdll.CloseUsbV12()
